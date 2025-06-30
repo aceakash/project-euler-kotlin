0a0aa140cf87989be163ed21624cdf0be5349802
Akash K <akashkurdekar@gmail.com>
2025-06-30 21:45:08 +0100

    problem 1

---
+++ src/problem001.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println(solve(3, 5, 1000))
+}
+
+fun solve(num1: Int, num2: Int, below: Int): Int {
+    var sum = 0
+    for (i in 1..below-1) {
+       if (i % num1 == 0 || i % num2 == 0) {
+           sum = sum + i
+       }
+    }
+    return sum
+}
+
+
