9928dbf37e924bf1cd182df80afc4912d2bbf4f3
Akash K <akashkurdekar@gmail.com>
2025-06-30 21:55:38 +0100

    problem 2

--- src/problem001.kt
+++ src/problem001.kt
@@ -1,8 +1,8 @@
 fun main() {
-    println(solve(3, 5, 1000))
+    println(problem001(3, 5, 1000))
 }
 
-fun solve(num1: Int, num2: Int, below: Int): Int {
+fun problem001(num1: Int, num2: Int, below: Int): Int {
     var sum = 0
     for (i in 1..below-1) {
        if (i % num1 == 0 || i % num2 == 0) {
---
+++ src/problem002.kt
@@ -0,0 +1,25 @@
+fun main() {
+    println(problem002(4_000_000))
+}
+
+fun problem002(below: Int): Long {
+    // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+    // sum of 1, 2, 3, 5, 8 =
+    var prev = 2
+    var value = 3
+
+    var sum = 2L
+    while (value < below) {
+        if (value % 2 == 0) {
+            sum += value
+        }
+
+        // Calculate the next Fibonacci number
+        val next = prev + value
+        prev = value
+        value = next
+    }
+    return sum
+}
+
+
